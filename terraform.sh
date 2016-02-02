5bdd9597c8dc005322bb76c8ab295a85e891c0e8
Ron Kurr <kurr@kurron.org>
2016-02-02 19:00:40 +0000

    Upgrade to current version

--- terraform.sh
+++ terraform.sh
@@ -6,13 +6,13 @@ CMD="docker run \
        --interactive \
        --tty \
        --net "host" \
-       --user 1000:1000 \
+       --user=$(id -u $(whoami)):$(id -g $(whoami)) \
        --volume $HOME:/home/developer \
        --volume $(pwd):/pwd \
        --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
        --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
        --env AWS_REGION=$AWS_REGION \
-       kurron/docker-terraform:latest"
+       kurron/docker-terraform:0.6.11"
 
 #echo $CMD
 eval $CMD $*
